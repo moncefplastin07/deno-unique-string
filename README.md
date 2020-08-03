01e9078b4dbf1187347394b8625a07e4ce11173e
Moncef Gaha <moncefplastin07@gmail.com>
2020-08-03 20:57:38 +0300

    First Verion��

---
+++ README.md
@@ -0,0 +1,1 @@
+Random and unique string or ID's for deno
---
+++ index.js
@@ -0,0 +1,10 @@
+
+export function uniqueString(len=10) {
+    let result = "";
+    let charactersList = "abcdefjhigklmnopqrstuvwxyzABCDEFJHIGKLMNOPQRSTUVWXYZ0123456789";
+    for(var char = 0; char < len; char++){
+         let randomCharIndex = Math.floor(Math.random() * charactersList.length);
+         result += charactersList[randomCharIndex];
+    }
+    return result;
+}
